8241a0efb3d7ecd150c33c8a93bf115528295dba
Yyghg276 <javiermunozalloza@gmail.com>
2025-04-22 19:40:57 +0200

    Initial commit

---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Egg : MonoBehaviour
+{
+    [Header(" Physics Settings ")]
+    [SerializeField] private float bounceVelocity;
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.TryGetComponent(out Player_Controller PlayerController))
+        {
+            Bounce(collision.GetContact(0).normal);
+        }
+    }
+    private void Bounce(Vector2 normal)
+    {
+        rig.velocity = normal * bounceVelocity;
+    }
+
+}
